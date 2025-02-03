35d89afd4138423f05d0b430b201b0a60c8a5735
Adam <23P0001@eng.asu.edu.eg>
2025-02-03 20:33:45 +0200

    Removed the retrying mechanism
    Retrying is already handled within GetGrades(), having two retry mechanisms caused overlapping due to the Timer interval being shorter than the time it takes to execute the retry mechanism in HttpHelper which is now removed.

--- Utilities/HttpHelper.cs
+++ Utilities/HttpHelper.cs
@@ -9,34 +9,16 @@ internal static class HttpHelper
 {
     internal static async Task<string> FetchPage(string url, HttpClient httpClient)
     {
-        var retryCount = 10; // Will retry up to 10 times
-        var retryDelay = 3000; // Initial delay (in ms) before trying again (increases linearily with each attempt; 3000, 6000, 9000)
-        var attempt = 0;
-
-        while (attempt < retryCount)
+        try
         {
-            try
-            {
-                Program.WriteLog(url, ConsoleColor.DarkGreen);
-                using var response = await httpClient.GetAsync(url).ConfigureAwait(false);
-                return response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync().ConfigureAwait(false) : throw new Exception(response.ReasonPhrase);
-            }
-            catch (Exception ex)
-            {
-                Program.WriteLog($"Exception (FetchPage): {ex.Message}", ConsoleColor.Red);
-
-                ++attempt;
-
-                // Once attempts limit is reached, throw the exception
-                if (attempt == retryCount)
-                {
-                    throw new Exception($"Failed to fetch page after {attempt} attempts. Last error: {ex.Message}", ex);
-                }
-
-                await Task.Delay(retryDelay * attempt).ConfigureAwait(false);
-            }
+            Program.WriteLog(url, ConsoleColor.DarkGreen);
+            using var response = await httpClient.GetAsync(url).ConfigureAwait(false);
+            return response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync().ConfigureAwait(false) : throw new Exception(response.ReasonPhrase);
         }
-
-        throw new Exception("Unreachable Code.");
+        catch (Exception)
+        {
+            Program.WriteLog("Exception (FetchPage)", ConsoleColor.Red);
+            throw;
+        }
     }
 }
