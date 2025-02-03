93d7e5bb3ebef9ae2f8a226a06c0cf6074ea881b
Adam <23P0001@eng.asu.edu.eg>
2025-02-04 00:07:07 +0200

    Improved logging

--- Utilities/HttpHelper.cs
+++ Utilities/HttpHelper.cs
@@ -7,17 +7,17 @@ namespace Grade_Monitor.Utilities;
 
 internal static class HttpHelper
 {
-    internal static async Task<string> FetchPage(string url, HttpClient httpClient)
+    internal static async Task<string> FetchPage(string url, HttpClient httpClient, ulong discordUserId)
     {
         try
         {
-            Program.WriteLog(url, ConsoleColor.DarkGreen);
+            Program.WriteLog($"{discordUserId}: {url}", ConsoleColor.DarkGreen);
             using var response = await httpClient.GetAsync(url).ConfigureAwait(false);
             return response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync().ConfigureAwait(false) : throw new Exception(response.ReasonPhrase);
         }
         catch (Exception)
         {
-            Program.WriteLog("Exception (FetchPage)", ConsoleColor.Red);
+            Program.WriteLog($"{discordUserId}: Exception (FetchPage)", ConsoleColor.Red);
             throw;
         }
     }
