fb66188b25d52aa7ea45cd33af31f57bcde2d1a0
Adam <23P0001@eng.asu.edu.eg>
2025-08-07 14:28:48 +0300

    No longer static, each user has their own instance containing their own cookies

--- Utilities/HttpHelper.cs
+++ Utilities/HttpHelper.cs
@@ -1,24 +1,47 @@
-﻿using System;
+﻿using Grade_Monitor.Core;
+using System;
+using System.Collections.ObjectModel;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Grade_Monitor.Core;
 
 namespace Grade_Monitor.Utilities;
 
-internal static class HttpHelper
+internal class HttpHelper
 {
-    internal static async Task<string> FetchPage(string url, HttpClient httpClient, ulong discordUserId)
+    private readonly CookieContainer _cookieContainer = new();
+    private readonly HttpClient _httpClient;
+    
+    internal HttpHelper() => _httpClient = new HttpClient(new HttpClientHandler { UseProxy = false, CookieContainer = _cookieContainer });
+
+    internal async Task<string> FetchPage(string url, ulong discordUserId)
     {
         try
         {
             Program.WriteLog($"{discordUserId}: {url}", ConsoleColor.DarkGreen);
-            using var response = await httpClient.GetAsync(url).ConfigureAwait(false);
+            using var response = await _httpClient.GetAsync(url).ConfigureAwait(false);
             return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
         }
         catch (Exception)
         {
-            Program.WriteLog($"{discordUserId}: Exception (FetchPage)", ConsoleColor.Red);
+            Program.WriteLog($"{discordUserId}: Exception (FetchPage - HttpClient)", ConsoleColor.Red);
             throw;
+        }
+    }
+
+    internal void SetCookie(string uri, string cookieHeader) => _cookieContainer.SetCookies(new Uri(uri), cookieHeader);
+
+    internal void SetCookies(ReadOnlyCollection<OpenQA.Selenium.Cookie> cookies)
+    {
+        foreach (var cookie in cookies)
+        {
+            var cookieName = cookie.Name;
+            if (cookieName == "XSRF-TOKEN")
+            {
+                continue;
+            }
+
+            SetCookie("https://eng.asu.edu.eg", $"{cookieName}={cookie.Value}");
         }
     }
 }
