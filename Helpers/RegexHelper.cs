f6815bf4d8b75d8527cc74dcb4511787a92cd888
Adam <23P0001@eng.asu.edu.eg>
2025-12-08 18:46:55 +0200

    Create RegexHelper.cs

---
+++ Helpers/RegexHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Grade_Monitor.Helpers;
+
+internal static partial class RegexHelper
+{
+    [GeneratedRegex(@"\b(Fall|Spring|Summer) \d{4}\b")]
+    internal static partial Regex SemesterNamePattern();
+}
