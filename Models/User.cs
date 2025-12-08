9fe519e7b51191682790f5642478fb5e38f9e02c
Adam <23P0001@eng.asu.edu.eg>
2025-12-08 17:43:08 +0200

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Grade_Monitor.Models;
+
+public class User
+{
+    public required ulong DiscordUserId { get; set; }
+    public required string StudentId { get; set; }
+    public required string Password { get; set; }
+    public string? LaravelSession { get; set; }
+    public Dictionary<string, HashSet<string>> Semesters { get; set; } = [];
+}
