cb3d6a6295633d94a9855440b63c98e6f9bc3a3a
Adam <23P0001@eng.asu.edu.eg>
2025-12-08 18:30:49 +0200

    Create AppPaths.cs

---
+++ Core/AppPaths.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Grade_Monitor.Core;
+
+internal static class AppPaths
+{
+    private static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+    internal static readonly string ChromeExe = Path.Combine(BaseDirectory, "chrome-win64", "chrome.exe");
+    internal static readonly string ChromeDriver = Path.Combine(BaseDirectory, "chromedriver-win64");
+
+    internal static readonly string Config = Path.Combine(BaseDirectory, "config.json");
+}
